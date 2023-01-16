d93f91181b9706cf5639a051c975a8fe33281cc0
jerripat <jerripatterson@gmail.com>
2023-01-15 20:50:33 -0600

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    fName= "Jerri Pat"
+    fav_pizza= ['peperoni', 'cheese', 'mushrooms', 'chilie']
+    return render_template('index.html',
+            f_name=fName, favPizza=fav_pizza)
